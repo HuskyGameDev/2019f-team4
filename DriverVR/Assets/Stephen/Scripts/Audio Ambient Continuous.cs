5be4ed080eef0c3f79769d784e599b49fb404f31
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2019-10-31 13:38:57 -0400

    Audio Handlers
    Generic & specialized for continuous audio handlers made.

---
+++ DriverVR/Assets/Stephen/Scripts/Audio Ambient Continuous.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAmbient : AudioHandler
+{
+    [SerializeField]
+    private AudioSource contSource;
+    private AudioClip[] contSounds;
+
+    private int contLength;
+    private int curSound;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        contLength = contSounds.Length;
+        curSound = rand.Next(contLength);
+        contSource.clip = contSounds[curSound];
+        Invoke("NextContinuousSound", contSounds[curSound].length+1f);
+        contSource.playOnAwake = true;
+        contSource.loop = false;
+    }
+
+    private void NextContinuousSound()
+    {
+        int nextSound = curSound;
+        while (nextSound == curSound)
+        {
+            nextSound = rand.Next(contLength);
+        }
+        curSound = nextSound;
+        contSource.PlayOneShot(contSounds[curSound]);
+        Invoke("NextContinuousSound", contSounds[curSound].length+1f);
+    }
+}
---
+++ DriverVR/Assets/Stephen/Scripts/Audio Handler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour
+{
+    protected System.Random rand;
+    [SerializeField]
+    private float frequency;
+    private AudioClip[] useSounds;
+    private AudioClip[] ambientSounds;
+    private AudioClip[] collisionSounds;
+    private AudioSource source;
+
+    private int ambientLength;
+    private int useLength;
+    private int collisionLength;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        ambientLength = ambientSounds.Length;
+        useLength = useSounds.Length;
+        collisionLength = collisionSounds.Length;
+        source.playOnAwake = false;
+        source.loop = false;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (rand.NextDouble() < frequency)
+        {
+            source.PlayOneShot(ambientSounds[rand.Next(ambientLength)]);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        source.PlayOneShot(collisionSounds[rand.Next(collisionLength)]);
+    }
+
+    private void Use()
+    {
+        source.PlayOneShot(useSounds[rand.Next(useLength)]);
+    }
+}
