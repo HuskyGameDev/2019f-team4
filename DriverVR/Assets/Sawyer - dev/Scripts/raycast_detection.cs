88a4044b69818d10f29b91c3464a7cea669e64f5
Sawyer Knowles <skdnowle@mtu.edu>
2019-12-05 13:40:56 -0500

    updated raycast b/w drone and player, added cone and trigger logic to spotlights

--- DriverVR/Assets/Sawyer - dev/Scripts/raycast_detection.cs
+++ DriverVR/Assets/Sawyer - dev/Scripts/raycast_detection.cs
@@ -6,20 +6,32 @@ public class raycast_detection : MonoBehaviour
 {
     
     public LayerMask mask;
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
 
-        Ray ray = new Ray(transform.position, transform.forward);
+        Vector3 dronePos = transform.position;
+        Vector3 playerPos = player.transform.position;
+        Vector3 direction = playerPos - dronePos;       
+
+        Ray ray = new Ray(dronePos, direction);
         RaycastHit hitInfo;
 
         if(Physics.Raycast(ray, out hitInfo, 100, mask)) {
-            print(hitInfo.collider.gameObject.name);
-            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            //print(hitInfo.collider.gameObject.name);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);    
         }
         else {
             Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
         }
+
+        /*if(Physics.Raycast(transform.position, direction, out hitInfo)) {
+            print("Hit Player");
+            Debug.DrawLine(transform.position, hitInfo.point, Color.green);
+        }*/
+
+
     }
 }
---
+++ DriverVR/Assets/Sawyer - dev/Scripts/trigger_detection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger_detection : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            print("PLAYER ENTERED CONE");
+        }
+        else {
+            print("NOT PLAYER");
+        }
+    }
+}
