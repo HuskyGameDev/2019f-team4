1530d51993714efaf608ff3ea269eb7ee4268c8c
Sawyer Knowles <skdnowle@mtu.edu>
2020-02-08 17:05:53 -0500

    added dection tracking framework to drones

--- DriverVR/Assets/Sawyer - dev/Scripts/trigger_detection.cs
+++ DriverVR/Assets/Sawyer - dev/Scripts/trigger_detection.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class trigger_detection : MonoBehaviour
 {
+    int detectMeter = 0;
+
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
             print("PLAYER ENTERED CONE");
+            
+            detectMeter += 25;
+            print(detectMeter);
+            if(detectMeter >= 100) {
+                print("GAME OVER");
+            }
+
         }
         else {
             print("NOT PLAYER");
