20ad1041d6bad51442f2dafa2baa40adecbd5653
SawyerKnowles <sdknowle@mtu.edu>
2020-04-20 19:12:43 -0400

    created car prefab, modified collect script

--- DriverVR/Assets/Sawyer - dev/Scripts/collect.cs
+++ DriverVR/Assets/Sawyer - dev/Scripts/collect.cs
@@ -7,19 +7,23 @@ public class collect : MonoBehaviour
 
     public GameObject player;
     public GameObject muffin;
+    public GameObject compass;
+    public GameObject home;
+    Compass compassScript;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player) {
             Destroy(gameObject);
             muffin.SetActive(true);
+            compassScript.Grannys = home.transform;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        compassScript = compass.GetComponent<Compass>();
     }
 
     // Update is called once per frame
