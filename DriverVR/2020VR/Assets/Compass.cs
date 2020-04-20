ba9b9933cb457c230abd9e67afd568ffede116df
basia <blkornoe@mtu.edu>
2020-04-20 16:04:07 -0400

    Compass
    Compass

---
+++ DriverVR/2020VR/Assets/Compass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    public Vector3 North;
+    public Transform Player;
+    public Quaternion GrandmasHouse;
+
+    public RectTransform Northlayer;
+    public RectTransform Missionlayer;
+
+    public Transform Grannys;
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeNorthDirection();
+        ChangeMissionDirection();
+
+    }
+
+    public void ChangeNorthDirection()
+    {
+        North.z = Player.eulerAngles.y;
+        Northlayer.localEulerAngles = North;
+
+    }
+
+    public void ChangeMissionDirection()
+    {
+        Vector3 direction = transform.position - Grannys.position;
+
+        GrandmasHouse = Quaternion.LookRotation(direction);
+
+        GrandmasHouse.z = GrandmasHouse.y;
+        GrandmasHouse.x = 0;
+        GrandmasHouse.y = 0;
+
+        Missionlayer.localRotation = GrandmasHouse * Quaternion.Euler(North);
+
+    }
+}
+
