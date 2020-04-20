c0f5e56c4ef8dd6cb9a338b035b4b49774462ffb
SawyerKnowles <sdknowle@mtu.edu>
2020-04-20 18:06:34 -0400

    added muffins

---
+++ DriverVR/Assets/Sawyer - dev/Scripts/spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 50 * Time.deltaTime, 0);
+    }
+}
