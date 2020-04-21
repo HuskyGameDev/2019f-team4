cab2dbe09d4b589b2ae6bc3a3958945989f0daff
SawyerKnowles <sdknowle@mtu.edu>
2020-04-20 21:03:21 -0400

    more map stuffs

---
+++ DriverVR/Assets/Semester2DevFolders/Basia/script/ImageShow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageShow : MonoBehaviour {
+    public bool ShowImage;
+    public GameObject Map;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("g") && ShowImage == false)
+        {
+            Map.SetActive(true);
+            ShowImage = true;
+        }
+        else if (Input.GetKeyDown("g") && ShowImage == true) {
+            Map.SetActive(false);
+            ShowImage = false;
+        }
+    }
+}
